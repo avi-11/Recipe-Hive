afa9f41cbdb3266420889f8a2c187c04ada16ded
avi-11 <singh.avi1109@gmail.com>
2023-07-25 19:54:24 +0530

    -- databinding --

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -6,9 +7,20 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
 
-  recipes:Recipe[]=[new Recipe("test Recipe","This is a simple test","https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Veggie-swirl-4a99ef8.jpg?quality=90&webp=true&resize=375,341"),
-  new Recipe("test Recipe","This is a simple test","https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Veggie-swirl-4a99ef8.jpg?quality=90&webp=true&resize=375,341")];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+   this.recipeWasSelected.emit(recipe);
+  }
 
 }
